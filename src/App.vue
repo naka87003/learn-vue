ad58a6ee9dcbfd0ad4dd6e8e8da66ae587f2e9dc
naka87003 <naka87003@outlook.jp>
2025-05-11 21:21:17 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ProductDisplay from "@/components/ProductDisplay.vue";
+
+const cart = ref(0);
+const premium = ref(true);
+</script>
+
+<template>
+  <div class="nav-bar"></div>
+  <div class="cart">Cart({{ cart }})</div>
+  <ProductDisplay :premium @addToCart="cart++" />
+</template>
